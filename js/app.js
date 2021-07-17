2255f05759c5ecf61bedc7f259990e128ddd20f6
Karim Ayman <86918608+Sayton7@users.noreply.github.com>
2021-07-17 02:20:21 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,70 +1,74 @@
 /*
-In this file we'll be using some techniques that weren't introduced in the course but learned through research
+In this project we have three main points specified in the project rubric Behavior section:
+1- Navigation is built dynamically as an unordered list.
+2- It should be clear which section is being viewed while scrolling through the page.
+3- When clicking an item from the navigation menu, the link should scroll to the appropriate section.
+
+We also have 5 points suggested in the Development Strategy concept:
+a- Add an active state to your navigation items when a section is in the viewport.
+b- Hide fixed navigation bar while not scrolling (it should still be present on page load).
+c- Add a scroll to top button on the page that’s only visible when the user scrolls below the fold of the page.
+d- Update/change the design/content.
+e- Make sections collapsible.
+
+I'll write the code for each point on its own if possible.
+
+Also I'll be using some techniques that weren't introduced in the course but learned through research
 like (template literals, arrow functions, ternary operator, ...)
 */
 
-// First things first. Selecting the page sections and adding them to the navigation menu.
 
+// 1- Navigation is built dynamically as an unordered list.
 
-// Selecting the unordered list at the header to use as a navigation menu.
+//selecting the unordered list at the header to use as a navigation menu.
 const navBar = document.querySelector("#navbar__list");
-
-// Selecting all sections in the page.
+//selecting all sections in the page.
 const sections = document.querySelectorAll("section");
-
-// Adding all sections a list items to the navigation menu.
-for (section of sections) {
+//adding all sections a list items to the navigation menu.
+sections.forEach(section => {
     const listItem = document.createElement('li');
     listItem.innerHTML = `<a href="#${section.id}" class="menu__link" data-nav="${section.id}">${section.dataset.nav}</a>`;
     navBar.appendChild(listItem);
-};
-
-// Second thing. Using the Intersection Observer API to activate a different style for the section in view.
-//Added the optional "Add an active state to your navigation items when a section is in the viewport."
-
-// Setting the options object
-const options = {
-    threshold: [0.25, 0.5, 0.75]
-};
-
-// Setting the callback function 
-const callback = (entries, observer) => {
-    entries.forEach(entry => {
-        let active__link = navBar.querySelector(`[data-nav="${entry.target.id}"]`);
-        if (entry.intersectionRatio > 0.7) {
-            entry.target.classList.add("your-active-class");
-            active__link.classList.add("active__link");
-        } else {
-            entry.target.classList.remove("your-active-class");
-            active__link.classList.remove("active__link");
-        }
-    }); 
-};
-
-const observer = new IntersectionObserver(callback, options);
-
-sections.forEach(section => {
-    observer.observe(section);
 });
 
+// 2- It should be clear which section is being viewed while scrolling through the page.
+// a- Add an active state to your navigation items when a section is in the viewport.
 
-// Third. adding the scrollIntoView option to the clicked links in the menu bar.
+const sectionObserver = new IntersectionObserver( (entries) => {
+    entries.forEach(entry => {
+        //selecting the corresponding navigation menu link to each section
+        let activeLink = document.querySelector(`[data-nav="${entry.target.id}"`);
+        //adding or removing classes that corresponds to different styles in the CSS file
+        if (entry.intersectionRatio > 0.25) {
+            entry.target.classList.add("your-active-class");
+            activeLink.classList.add("active__link");
+        } else {
+            entry.target.classList.remove("your-active-class");
+            activeLink.classList.remove("active__link");
+        }
+    })
+},
+// setting the threshold option
+{threshold: [0.25, 0.75]});
+//calling the IntersectionObserver on each section
+for (section of sections) {
+sectionObserver.observe(section);
+};
 
+// 3- When clicking an item from the navigation menu, the link should scroll to the appropriate section.
 
 navBar.addEventListener("click", (listItem) => {
-    // This is to prevent the default scroll behavior
+    //prevent default jump into view action for the click
     listItem.preventDefault();
-
     if (listItem.target.dataset.nav) {
         let selectedSection = document.querySelector(`#${listItem.target.dataset.nav}`);
         selectedSection.scrollIntoView({ behavior: "smooth", block: "center", inline: "start"});
     }
 });
 
-// Now to hide the navigation menu while  not scrolling.
+// b- Hide fixed navigation bar while not scrolling (it should still be present on page load).
 
 let navigationMenu = document.querySelector(".navbar__menu");
-
 document.onscroll = () => {
     let timer;
     clearTimeout(timer)
@@ -72,13 +76,12 @@ document.onscroll = () => {
     timer = setTimeout(() => {
         navigationMenu.style.display = "none";
     }, 5000);
-
     // This line is related to the scroll to top button at line: 109
     // Using the ternary operator which was implemented in ES6 instead of a normal if statement
     window.scrollY > 1000 ? (scrollButton.style.display = "block") : (scrollButton.style.display = "none");
 };
 
-// Adding a bonus option to hover over the top of the page to view the navigation bar again without the need of scrolling.
+// Adding a bonus hover to show option.
 
 document.onmousemove = (mousePosition) => {
     let timer;
@@ -91,21 +94,6 @@ document.onmousemove = (mousePosition) => {
         }, 5000);
     }
 };
-
-// Then, making the sections collapsible;
-
-let buttons = document.querySelectorAll(".collapse");
-
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        let coll = document.querySelector(`[data-coll="${button.id}"]`);
-        if (coll.style.display === "block") {
-            coll.style.display = "none";
-        } else {
-            coll.style.display = "block"
-        };
-    });
-});
 
 // Adding a scroll to top button
 
@@ -120,6 +108,23 @@ scrollButton.addEventListener("click", () => {
     }
     window.scrollTo(scrollToTop);
 });
+// We need to use an (onscoll) event listener but we already have one at line: 72.
+// To avoid duplication the excution line was placed at line: 81.
 
-// We need to use an (onscoll) event listener but we already have one at line: 68.
-// To avoid duplication the excution line was placed at line: 78.
+// e- Make sections collapsible.
+
+let buttons = document.querySelectorAll(".collapse");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        let coll = document.querySelector(`[data-coll="${button.id}"]`);
+        if (coll.style.display === "block") {
+            coll.style.display = "none";
+        } else {
+            coll.style.display = "block"
+        };
+    });
+});
+
+// d- Update/change the design/content.
+// changes were made in the CSS file.
